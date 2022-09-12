54da874c71164d579c35a76146d4597d15ef048b
Johnnyy91 <claudiosardilli@gmail.com>
2022-09-12 16:38:07 +0200

    elaboration file 2

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@ const first = new Vue(
     {
         el: '#root',
         data: {
-            text: 'Hello World',
+            text: 'HELLO VUE . JS',
             image: 'img/01.jpg',
             image2: 'img/02.jpg',
             image3: 'img/03.jpg',
             image4: 'img/04.jpg',
             image5: 'img/05.jpg',
-            variation: 'red big center'
+            redBig: 'red big center',
+            blueBig: 'blue big center',
+            greenBig: 'green big center'
+
         }
     }
 );
