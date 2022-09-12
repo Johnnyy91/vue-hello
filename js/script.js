368ea16ed3fb99264a36be1f0c5863d17d1045ab
Johnnyy91 <claudiosardilli@gmail.com>
2022-09-12 16:29:25 +0200

    elaboration file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 console.log('JS-OK')
 
-const app = new Vue(
+const first = new Vue(
     {
         el: '#root',
         data: {
             text: 'Hello World',
-            img: 'image'
+            image: 'img/01.jpg',
+            image2: 'img/02.jpg',
+            image3: 'img/03.jpg',
+            image4: 'img/04.jpg',
+            image5: 'img/05.jpg',
+            variation: 'red big center'
         }
     }
 );
+
